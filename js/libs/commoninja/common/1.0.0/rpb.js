5e3fdc716e61aa9a460ff4cdc7356662135fbba9
cavaluis <95144639+cavaluis@users.noreply.github.com>
2025-05-15 11:01:13 -0600

    Update rpb.js

--- js/libs/commoninja/common/1.0.0/rpb.js
+++ js/libs/commoninja/common/1.0.0/rpb.js
@@ -1,1 +1,18 @@
+const tryRemoveRibbon = () => {
+  const host = document.querySelector('.commonninja_component');
+  if (!host) return;
 
+  const shadowRoot = host.shadowRoot;
+  if (!shadowRoot) return;
+
+  const ribbon = shadowRoot.querySelector('.commonninja-ribbon');
+  if (ribbon) {
+    ribbon.remove();
+    console.log('Ribbon eliminado ✅');
+    clearInterval(checkInterval); // detener una vez eliminado
+  } else {
+    console.log('Ribbon aún no encontrado...');
+  }
+};
+
+const checkInterval = setInterval(tryRemoveRibbon, 500);
